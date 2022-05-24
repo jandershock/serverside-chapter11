6d2b9852df08260a7e65d306241e135ada156fa4
jandershock <jordan.andershock@gmail.com>
2022-05-24 11:25:46 -0500

    Finished chapter 11 company exercise

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Classes
+{
+    public class Employee
+    {
+        public String FirstName {get;set;}
+        public String LastName {get;set;}
+        public String Title {get;set;}
+        public DateTime StartDate {get;set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company krustyKrab = new Company("The Krusty Krab", DateTime.Now);
+
+            // Create three employees
+            List<Employee> employeeList = new List<Employee>(){
+                new Employee(){
+                    FirstName = "Eugene",
+                    LastName = "Krabs",
+                    Title = "Owner",
+                    StartDate = DateTime.Now
+                },
+                new Employee() {
+                    FirstName = "Squidward",
+                    LastName = "Tentacles",
+                    Title = "Cashier",
+                    StartDate = DateTime.Now
+                },
+                new Employee() {
+                    FirstName = "SpongeBob",
+                    LastName = "SquarePants",
+                    Title = "Frycook",
+                    StartDate = DateTime.Now
+                }
+            };
+
+            // Assign the employees to the company
+            krustyKrab.Employees.AddRange(employeeList);
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+            krustyKrab.ListEmployees();
+        }
+    }
+
+
+}
